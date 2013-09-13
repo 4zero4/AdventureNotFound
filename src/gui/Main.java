af28a93df9ac39fab9ebe3d95c93cbe7384ecd22
Omella <bruno.omella@hotmail.com>
2013-09-12 23:49:12 -0300

    Tis not working. Should run an attibute test.
    "Should" being the key word. Right now it doesn't. It does nothing. It
    breaks.

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,11 +1,49 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
+import omella.Adventurer;
+
 public class Main {
+	
 	public static void main(String args[]) {
+		final Adventurer juvigar = null;
 		JFrame hans = new JFrame("Adventure Not Found");
 		hans.setVisible(true);
 		hans.setSize(800, 600);
 		hans.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton hashtag = new JButton("Create Your Juvigar");
+		hashtag.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e)
+            {
+                instancia();
+                System.out.println("Your Juvigar has been created!");
+                juvigar.checkAtt();
+		        juvigar.reRoll();
+		        juvigar.checkAtt();
+            }			
+		}
+		);
+		hans.getContentPane().add(hashtag);
+		/*JButton partiu = new JButton("Roll me some attributes!");
+		partiu.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e)
+            {
+				juvigar.checkAtt();
+		        juvigar.reRoll();
+		        juvigar.checkAtt();
+            }			
+		}
+		);
+		hans.getContentPane().add(partiu);*/
 	}
+	
+	public static void instancia(){
+		Adventurer juvigar = new Adventurer("Juvigar",1,2);
+	}
+
+	
 }
