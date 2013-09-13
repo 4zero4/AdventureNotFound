a04a84173efdc44447f9f11643c3e0be1e28c575
RunoFawkes <i.am.runo@gmail.com>
2013-09-12 23:10:27 -0300

    Put something on the gui Package
    Simple JFrame as initial GUI

---
+++ src/gui/Main.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+public class Main {
+	public static void main(String args[]) {
+		JFrame hans = new JFrame("Adventure Not Found");
+		hans.setVisible(true);
+		hans.setSize(800, 600);
+		hans.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
